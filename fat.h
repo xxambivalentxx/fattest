cc3fcd88b780309a1e89bbc16df8c21f350855c6
xxambivalentxx <jspaulse@ius.edu>
2016-01-19 21:07:25 -0500

    first commit

---
+++ fat.h
@@ -0,0 +1,159 @@
+#ifndef FAT32_H
+#define FAT32_H
+#include <stdint.h>
+
+#define FAT_PARTITION_OFFSET	0x1BE
+/* first sector, LBA = 0 */
+/* FAT_PART_16_SMALL is < 32MiB */
+/* FAT_PART_32_SMALL is < 2GiB  */
+/* think we'll only support FAT_PART_16/32_LBA */
+#define FAT_TYPE_UNUSED			0
+#define FAT_TYPE_FAT12			1
+#define FAT_TYPE_FAT16_SMALL	4
+#define FAT_TYPE_EXT			5
+#define FAT_TYPE_16_NO_LBA		6
+#define FAT_TYPE_32_SMALL		11
+#define FAT_TYPE_32				12
+#define FAT_TYPE_16				14
+#define FAT_TYPE_EXT_LBA		15
+/**
+ * @fat_bpb
+ * 
+ * File Allocation Bios Parameter Block
+ * 
+ * @jmp						jump instruction; can be ignored
+ * @oem_id					oem identification; can be ignored
+ * @bytes_per_sector		generally 512
+ * @sectors_per_cluster		
+ * @reserved_sector_cnt		
+ * @fat_cnt					number of file allocation tables
+ * @dir_entries_cnt			number of directory entries
+ * @total_sectors			number of sectors on disk/partition; if 0, refer to large_total_sectors
+ * @media_descriptor_type	
+ * @sectors_per_fat_16_12	sectors per fat table; disregard if using fat32
+ * @sectors_per_track
+ * @heads_sides_cnt			number of heads or sides on media; can probably be ignored (BECAUSE IT IS 2016!)
+ * @hidden_sector_cnt		number of hidden sectors
+ * @large_total_sectors		refer here if total_sectors is 0
+ **/	
+struct fat_bpb {
+	uint8_t		jmp[3];					/* jmp instruction, disregard */
+	uint8_t		oem_id[8];				/* oem identifier, can be disregarded */
+	uint16_t	bytes_per_sector;
+	uint8_t		sectors_per_cluster;
+	uint16_t	reserved_sector_cnt;
+	uint8_t		table_cnt;				/* number of File Allocation Tables */
+	uint16_t	dir_entries_cnt;		/* number of directory entries, disregard on fat32 */
+	uint16_t	total_sector_cnt;		/* refer to larger entry if zero */
+	uint8_t		media_descriptor_type;	/* whaddafuck? */
+	uint16_t	sectors_per_fat16;		/* reserved for fat12/16 */
+	uint16_t	sectors_per_track;		/* whaddafuck? */
+	uint16_t	heads_sides_cnt;		/* number of heads/sides (dunno) */
+	uint32_t	hidden_sector_cnt;
+	uint32_t	large_total_sector_cnt;	/* refer here if total_sectors is 0. */
+} __attribute__ ((packed));
+
+/**
+ * fat16_ebpb
+ * 
+ * Fat16 Extended Bios Parameter Block
+ * 
+ * @bpb					Bios Parameter Block
+ * @drive_number		0x13 floppy, 0x80 hdd
+ * @win_nt_flags		can be ignored
+ * @signature			must be 0x28/0x29
+ * @volumeid_serial		volume id 'serial' number; can be ignored
+ * @volume_label		label of the volume/partition
+ * @system_id			can be ignored?
+ * @boot_code			boot code stuff
+ * @boot_signature		must be 0xAA55
+ **/
+struct fat16_extbpb {
+	struct fat_bpb 	bpb;
+	uint8_t			drive_number;
+	uint8_t			win_nt_flags;
+	uint8_t			signature;
+	uint32_t		volumeid_serial;
+	uint8_t			volume_label[11];
+	uint8_t			system_id[8];
+	uint8_t			boot_code[448];
+	uint16_t		bootable_signature;
+} __attribute__ ((packed));
+	
+
+/**
+ * fat32_ebpb
+ * 
+ * Fat32 Extended Bios Parameter Block
+ * 
+ * @bpb						Bios Parameter Block
+ * @sectors_per_fat			size of file allocation table in sectors
+ * @flags				
+ * @version					high bytes is major, low byte is minor
+ * @root_dir_cluster_number	cluster number of root directory
+ * @fs_info_sector			sector number of fsinfo structure
+ * @backup_boot_sector		sector number of backup boot sector
+ * @reserved
+ * @drive_number			0x13 floppy, 0x80 hdd
+ * @win_nt_flags			can be ignored
+ * @signature				must 0x28/0x29
+ * @volumeid_serial			volume id 'serial' number; can be ignored
+ * @volume_label			volume label string
+ * @system_id				system_id, generally states "FAT32" can be ignored?
+ * @boot_code				boot code stuff
+ * @bootable_signature		always 0xAA55
+ **/
+struct fat32_extbpb {
+	struct fat_bpb		bpb;
+	uint32_t			sectors_per_fat;	
+	uint16_t			flags;
+	uint8_t				version[2];
+	uint32_t			root_cluster;
+	uint16_t			fs_info_sector;			
+	uint16_t			backup_boot_sector;
+	uint8_t				reserved[12];
+	uint8_t				drive_number;
+	uint8_t				win_nt_flags;			/* disregard */
+	uint8_t				signature;				/* must be 0x28/0x29 */ 
+	uint32_t			volumeid_serial;		
+	uint8_t				volume_label[11];
+	uint8_t				system_id[8];
+	uint8_t				boot_code[420];
+	uint16_t			bootable_signature;		/* 0xAA55 */
+} __attribute__ ((packed));
+
+/**
+ * fat_partition
+ * @boot_flags		can be ignored
+ * @start_chs		can be ignored
+ * @type			FAT_PART_XX, see above
+ * @end_chs			can be ignored
+ * @start_sector	starting sector of partition
+ * @size			size (in sectors) of partition
+ **/
+struct fat_partition {
+	uint8_t		boot_flag;
+	uint8_t		start_chs[3];
+	uint8_t		type;
+	uint8_t		end_chs[3];
+	uint32_t	start_sector;
+	uint32_t	size;
+} __attribute__ ((packed));
+
+struct dir_entry {
+	uint8_t		entry_name[8];
+	uint8_t		ext[3];
+	uint8_t		attrb;
+	uint8_t		win_nt_reserved;
+	uint8_t		creation_time_tenths;
+	uint16_t	creation_time;			/* hour:minutes:seconds, 5b:6b:5b */
+	uint16_t	creation_date;			/* year:month:day, 7b:4b:5b */
+	uint16_t	last_access_date;		/* ^^^ this format */
+	uint16_t	cluster_high;
+	uint16_t	last_mod_time;			/* creation time format */
+	uint16_t	last_mod_date;			/* creation date format */
+	uint16_t	cluster_low;
+	uint32_t	size;
+} __attribute__ ((packed));
+
+#endif
---
+++ test.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <byteswap.h>
+#include "fat.h"
+#define GENERIC_SECTOR_SIZE	512
+
+/**
+ * cluster_to_sector
+ * 
+ * calculates the beginning sector to a cluster
+ * 
+ * @rel_start_clust			relative_start_cluster, starting cluster in a partition
+ * @clust_num				requested cluster to sector number
+ * @sect_per_clust			number of sectors per cluster in partition
+ *
+ * @return beginning sector to a cluster
+ **/
+uint32_t cluster_to_sector(uint32_t rel_start_clust, uint32_t clust_num, uint32_t sect_per_clust) {
+	return rel_start_clust + (clust_num - 2) * sect_per_clust;
+}
+/**
+ */
+struct fs_info {
+	/* both relative_start_cluster/sector are relative to the start of the partition */
+	uint8_t			fs_type;					/* defined as FS_TYPE_XX */
+	uint32_t		fat_start_sector;
+	uint32_t		root_dir_cluster;
+	uint32_t		sectors_per_fat;			/* can be pull directly from structure */
+	uint32_t		sectors_per_cluster;		/* can be pull directly from structure */
+	uint32_t		root_cluster;				/* can be pull directly from structure */
+	uint32_t		bytes_per_sector;			/* can be pull directly from structure */
+};
+/*
+ * if partition type is anything but 12 (or whatever fat32 is) throw a fucking fit.
+ * 
+ * "relative linear offset to the start of the partition. "
+ */
+//lba_addr = cluster_begin_lba + (cluster_number - 2) * sectors_per_cluster; ANY SECTOR
+int main() {
+	FILE *fp 	= NULL;// = fopen("rasp.img", "rb");
+	/* can add pointers like (struct bullshit_t *)(buf + OFFSET); */
+	
+	if ((fp = fopen("/dev/sdb", "rb")) != NULL) {
+		/* might end up in a structure */
+		struct fat32_extbpb extbpb;
+		struct fat_partition fpt;
+		struct dir_entry de;
+		struct fs_info fs;
+		unsigned int root_directory = 0;
+		unsigned int first_data_sector = 0;
+		/* "fat_size" is either bpb.sectors_per_fat16 if fat16 or extbpb.sectors_per_fat */
+		unsigned int fat_size = 0;
+		
+		fseek(fp, FAT_PARTITION_OFFSET, SEEK_SET);
+		fread(&fpt, sizeof(struct fat_partition), 1, fp);
+		
+		/* calculate next offset */
+		fseek(fp, GENERIC_SECTOR_SIZE * fpt.start_sector, SEEK_SET);
+		fread(&extbpb, sizeof(struct fat32_extbpb), 1, fp);
+		
+		fs.fs_type					= fpt.type;
+		fs.bytes_per_sector			= extbpb.bpb.bytes_per_sector;			/* this is mostly for testing purposes */
+		fs.sectors_per_fat			= extbpb.sectors_per_fat;				/* this would be extbpb.bpb.sectors_per_fat16 if fat 16 */
+		fs.sectors_per_cluster		= extbpb.bpb.sectors_per_cluster;
+		fs.root_cluster				= extbpb.root_cluster;
+		fs.fat_start_sector			= fpt.start_sector + extbpb.bpb.reserved_sector_cnt;
+		fs.root_dir_cluster			= fpt.start_sector + extbpb.bpb.reserved_sector_cnt + (extbpb.bpb.table_cnt * fs.sectors_per_fat);
+		
+		/* relative_start_sector should be the fat sector? if this is the case, we should see a reference in slot #2 of nada (0xFFFFFFFF?) */
+		fseek(fp, fs.bytes_per_sector * fs.relative_start_sector, SEEK_SET);
+		unsigned int table[512];
+		fread(&table, 4 * 512, 1, fp);
+		for (int i = 0; i < 5; i++) {
+			printf("[%i]: 0x%x\n", i, table[i]);
+		}
+		
+		printf("[0x98]: 0x%x\n", table[0x98]);
+		fseek(fp, cluster_to_sector(fs.relative_start_cluster, fs.root_cluster, fs.sectors_per_cluster) * fs.bytes_per_sector, SEEK_SET);
+		
+		for (int i = 0; i < 5; i++) {
+			fread(&fe, sizeof(struct fat_entry), 1, fp);
+	
+			printf("[%s].[%s]\n", fe.entry_name, fe.ext);
+			printf("attb: 0%x\n", fe.attrb);
+			printf("size: %i\n", fe.size);
+			printf("high: 0x%x\n", fe.cluster_high);
+			printf("low: 0x%x\n", fe.cluster_low);
+			
+			/* first fat sector is after reserved sectors? first_fat_sector = extbpb.bpb.reserved_sector_cnt 
+			* Every sector of of the FAT holds 128 of these 32 bit integers, so looking up the next 
+			 * cluster of a file is relatively easy. Bits 7-31 of the current cluster tell you which sectors to read from the FAT, and bits 
+			 * 0-6 tell you which of the 128 integers in that sector contain is the number of the next cluster of your file (or if all ones, that the current cluster is the last).
+			 */
+		}
+		
+		printf("gonna try and read text\n");
+		fseek(fp, cluster_to_sector(fs.relative_start_cluster, 0x98, fs.sectors_per_cluster) * fs.bytes_per_sector, SEEK_SET);
+		char test[33];
+		fread(&test, 32, 1, fp);
+		test[32] = '\0';
+		printf("test: %s\n", test);
+	} else {
+		printf("unable to open img!\n");
+	}
+	
+	fclose(fp);
+	
+	return 0;
+}
+
